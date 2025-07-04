079408e7fb8e92fe1fc8f4d9dcd6ca0b126a2cc6
Julius-Nganga <hingamax@gmail.com>
2025-07-05 02:24:35 +0300

    thirty now

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const REPO_NAME = 'react-tailwind-app';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()]
