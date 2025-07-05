a39e3a3a2167306049ca71b2cc9ea1a8778345dd
Julius-Nganga <hingamax@gmail.com>
2025-07-05 10:09:24 +0300

    3rd commit

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTheme } from '../context/ThemeContext';
-import Button from '../components/button';
+import Button from './button';
 
 const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
