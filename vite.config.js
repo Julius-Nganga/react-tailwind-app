fa020c2bc819084bf0638c2c6e8bee46cf6279bc
Julius-Nganga <hingamax@gmail.com>
2025-07-05 04:40:33 +0300

    I did not sleep

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ const REPO_NAME = 'react-tailwind-app';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
-           
+  plugins: [react(), tailwindcss()],
+  base: `/${REPO_NAME}/`,
 });
